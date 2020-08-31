8f59e6cbf5f48ed671f4a77d35d56f5abdc92113
Gokhan <gokhanarkan@gmail.com>
2020-08-31 15:13:19 +0300

    copy-text feature

--- src/App.vue
+++ src/App.vue
@@ -9,9 +9,18 @@
           <div v-if="showInfo" class="border-b border-gray-200 px-4 py-5 sm:px-6">
             <Info :show="showInfo" />
           </div>
-          <div class="px-4 py-5 sm:p-6">
+          <div class="px-4 py-5 sm:p-6 sm:pb-2 mb-10">
             <Dropzone ref="dropzone" @get-response="getResponse" />
             <TextArea :text="text" />
+            <span v-if="showButton" class="inline-flex rounded-md shadow-sm mt-2 float-right">
+            <button v-clipboard:copy="text" type="button"
+                class="inline-flex items-center px-2 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-white bg-indigo-400 hover:bg-indigo-500 focus:outline-none focus:border-indigo-500 focus:shadow-outline-indigo transition ease-in-out duration-150">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="-ml-0.5 mr-2 h-4 w-4">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 5H6a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2v-1M8 5a2 2 0 002 2h2a2 2 0 002-2M8 5a2 2 0 012-2h2a2 2 0 012 2m0 0h2a2 2 0 012 2v3m2 4H10m0 0l3-3m-3 3l3 3" />
+                </svg>
+                Copy Text
+            </button>
+            </span>
             </div>
             <div class="border-t border-gray-200 px-4 py-4 sm:px-6">
               <Buttons @remove-file="removeFiles" :showButton="showButton" />
@@ -56,6 +65,7 @@
       },
       removeFiles() {
         this.$refs.dropzone.removeFile();
+        this.showButton = false;
       }
     }
   }
--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueClipboard from "vue-clipboard2";
 
 import "./assets/styles/index.css";
 
 Vue.config.productionTip = false
+
+Vue.use(VueClipboard);
 
 new Vue({
   render: h => h(App),
